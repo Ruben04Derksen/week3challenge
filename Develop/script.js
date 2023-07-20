a84334cf2cb1a7632da704946e2f16082edade90
Ruben Derksen <ruben@Rubens-MacBook-Pro.local>
2023-07-20 14:03:39 +0200

    homework stuff

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword() {
+  return Math.random()*100000000000000000;
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
