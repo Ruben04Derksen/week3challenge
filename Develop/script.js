bc44a9334d2a896dc6aad6e79edc05e68eb44686
Ruben <rderksen04@outlook.com>
2023-08-21 16:13:27 -0700

    revision

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var alpha = ["a", "b", 'c', 'd', "'e", "f", "g", "h", "i", "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 // Write password to the #password input
 function generatePassword() {
-  return Math.random()*100000000000000000;
+  var password = "";
+  let length = alpha.length
+  for(var i = 0; i < 10;i++)
+  {
+    password += alpha[Math.floor(Math.random()*length)];
+  }
+  return password + Math.random()*100000000000000000;
 }
 function writePassword() {
   var password = generatePassword();
